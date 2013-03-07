16b8705188815f160c7d1353695131713ed95b14
dj rosenbaum <djrosenbaum@gmail.com>
2013-03-07 00:55:39 -0500

    update to tbg

---
+++ js/actions.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	//begin ready
+	console.log('hello tournament');
+
+	$('.add-bracket').on('click',function() {
+		console.log('add a bracket');
+	});
+	//end ready
+});
