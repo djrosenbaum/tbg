4084b43215cd43e0847925e3a505da9554d84959
dj rosenbaum <djrosenbaum@gmail.com>
2013-03-08 20:30:35 -0500

    building round 1

--- js/actions.js
+++ js/actions.js
@@ -2,8 +2,16 @@ $(document).ready(function() {
 	//begin ready
 	console.log('hello tournament');
 
-	$('.add-bracket').on('click',function() {
-		console.log('add a bracket');
+	$('.round-1-bracket .round-1-player-top').on('click',function() {
+		console.log('edit player top');
+		var player_name = $(this).children("p").text();
+		var input_player_name = $(this).children("input").val();
+		console.log(player_name);
+		console.log(input_player_name);
+		$(this).children("p").hide();
+		$(this).children("input").show();
 	});
+
+
 	//end ready
 });
