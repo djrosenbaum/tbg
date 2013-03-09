0b2b8fa7e73160b816ba33737ae9ad72badb302d
dj rosenbaum <djrosenbaum@gmail.com>
2013-03-09 05:41:24 -0500

    round 1 bracket functionality
    functionality for updating player name with save button

--- js/actions.js
+++ js/actions.js
@@ -2,14 +2,29 @@ $(document).ready(function() {
 	//begin ready
 	console.log('hello tournament');
 
-	$('.round-1-bracket .round-1-player-top').on('click',function() {
+	$('.round-1-bracket .round-1-player-top .player-name').on('click',function() {
 		console.log('edit player top');
 		var player_name = $(this).children("p").text();
-		var input_player_name = $(this).children("input").val();
 		console.log(player_name);
+		$(this).addClass('hidden');
+		$(this).siblings(".player-input").removeClass('hidden');
+/*		var input_player_name = $(this).find("input").val();
 		console.log(input_player_name);
-		$(this).children("p").hide();
-		$(this).children("input").show();
+
+*/
+	});
+
+	$('.save-button').on('click',function() {
+		console.log('save button was clicked');
+		var player_name = $(this).siblings("input").val();
+		$(this).siblings("input").attr('value', player_name);
+		console.log(player_name);
+		var new_player_name = $(this).parent().siblings('.player-name').children('p').text();
+		console.log(new_player_name);
+		console.log(new_player_name);
+		$(this).parent('.player-input').addClass('hidden');
+		$(this).parent().siblings('.player-name').children('p').text(player_name);
+		$(this).parent().siblings('.player-name').removeClass('hidden');
 	});
 
 
