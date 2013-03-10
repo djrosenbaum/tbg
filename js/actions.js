68c5fe872ec7c1fca5b79b937b5e041e76c9e8c7
dj rosenbaum <djrosenbaum@gmail.com>
2013-03-09 23:01:07 -0500

    round 1 player name editing

--- js/actions.js
+++ js/actions.js
@@ -2,31 +2,34 @@ $(document).ready(function() {
 	//begin ready
 	console.log('hello tournament');
 
-	$('.round-1-bracket .round-1-player-top .player-name').on('click',function() {
-		console.log('edit player top');
-		var player_name = $(this).children("p").text();
-		console.log(player_name);
-		$(this).addClass('hidden');
-		$(this).siblings(".player-input").removeClass('hidden');
-/*		var input_player_name = $(this).find("input").val();
-		console.log(input_player_name);
+	/* functions */
 
-*/
-	});
+	function edit_player_name() {
+		//console.log('edit player top');
+		var player_name = $(this).text();
+		$(this).parent('.player-name').siblings(".player-input").children("input").attr('value', player_name);
+		//console.log(player_name);
+		$(this).parent('.player-name').addClass('hidden');
+		$(this).parent('.player-name').siblings(".player-input").removeClass('hidden');		
+	}
 
-	$('.save-button').on('click',function() {
-		console.log('save button was clicked');
+	function save_player_name() {
+		//console.log('save button was clicked');
 		var player_name = $(this).siblings("input").val();
 		$(this).siblings("input").attr('value', player_name);
-		console.log(player_name);
+		//console.log(player_name);
 		var new_player_name = $(this).parent().siblings('.player-name').children('p').text();
-		console.log(new_player_name);
-		console.log(new_player_name);
-		$(this).parent('.player-input').addClass('hidden');
+		//console.log(new_player_name);
 		$(this).parent().siblings('.player-name').children('p').text(player_name);
-		$(this).parent().siblings('.player-name').removeClass('hidden');
-	});
+		$(this).parent('.player-input').addClass('hidden');
+		$(this).parent().siblings('.player-name').removeClass('hidden');		
+	}
 
+	/* event listeners */
+
+	$('.player-name p').click(edit_player_name);
+
+	$('.save-button').click(save_player_name);
 
 	//end ready
 });
